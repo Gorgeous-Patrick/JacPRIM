3c5d9e40c4bf6b754a3899879629797f7c462321
Patrick Li <ilovegithub@patrickli.mozmail.com>
2025-04-14 19:43:42 +0000

    cmake list added

--- hist/dpu/task.c
+++ hist/dpu/task.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include <dpu.h>
 static void histogram(uint32_t* histo, uint32_t bins, T *input, uint32_t histo_id, unsigned int l_size){
     for(unsigned int j = 0; j < l_size; j++) {
         T d = (input[j] * bins) >> DEPTH;
